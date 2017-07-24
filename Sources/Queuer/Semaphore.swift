35dd6c8c174ae8325788611352a3e2ca32306120
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2017-07-24 11:50:01 +0200

    Adding `init(poolSize:)`

--- Sources/Queuer/Semaphore.swift
+++ Sources/Queuer/Semaphore.swift
@@ -30,7 +30,19 @@ import Dispatch
 /// DispatchSemaphore struct wrapper.
 public struct Semaphore {
     /// Private DispatchSemaphore.
-    private let semaphore = DispatchSemaphore(value: 0)
+    private let semaphore: DispatchSemaphore
+    
+    /// Creates new counting semaphore with an initial value.
+    /// Passing zero for the value is useful for when two threads need to reconcile
+    /// the completion of a particular event. Passing a value greater than zero is
+    /// useful for managing a finite pool of resources, where the pool size is equal
+    /// to the value.
+    ///
+    /// - Parameter poolSize: The starting value for the semaphore.
+    ///                       Passing a value less than zero will cause NULL to be returned.
+    public init(poolSize: Int = 0) {
+        self.semaphore = DispatchSemaphore(value: poolSize)
+    }
     
     /// Wait for a `continue` function call.
     @discardableResult
