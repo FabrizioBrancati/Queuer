1e31b95ce227b261b92d9bae6ee9bcb3b15a7022
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2017-07-25 10:29:40 +0200

    Removing RequestOperation test on Linux

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -6,7 +6,6 @@ import XCTest
 XCTMain([
     testCase(ConcurrentOperationTests.allTests),
     testCase(QueuerTests.allTests),
-    testCase(RequestOperationTests.allTests),
     testCase(URLBuilderTests.allTests),
     testCase(SemaphoreTests.allTests),
     testCase(SynchronousOperationTests.allTests)
