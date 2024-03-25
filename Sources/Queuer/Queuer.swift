f3f47db834a84628295e1d32591576bd1d31ae8c
Fabrizio Brancati <fabrizio.brancati@truecaller.com>
2024-03-25 17:58:31 +0200

    Update license year to 2024

--- Sources/Queuer/Queuer.swift
+++ Sources/Queuer/Queuer.swift
@@ -4,7 +4,7 @@
 //
 //  MIT License
 //
-//  Copyright (c) 2017 - 2021 Fabrizio Brancati
+//  Copyright (c) 2017 - 2024 Fabrizio Brancati
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
