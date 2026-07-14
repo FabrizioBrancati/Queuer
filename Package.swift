9c5f41c2cf8e452f7e945a67c192860fe9903de5
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2026-07-14 17:38:55 +0200

    chore: move to swiftlang repo

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         .library(name: "Queuer", targets: ["Queuer"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.5.0")
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.5.0")
     ],
     targets: [
         .target(
